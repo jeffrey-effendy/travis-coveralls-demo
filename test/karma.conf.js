03e0225bf866590347acdb94c9cb5c0acee3ba9d
Jeffrey Effendy <wawa_union.zone@live.com>
2016-02-01 01:50:40 +0800

    edit travis

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,7 +2,8 @@
 // Generated on Sun Jan 31 2016 20:00:44 GMT+0800 (Malay Peninsula Standard Time)
 
 module.exports = function(config) {
-  config.set({
+  
+  var configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -67,8 +68,15 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: ['Chrome', 'ChromeCanary'],
+    
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+    
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -77,5 +85,11 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  })
+  };  
+  
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+  }  
+  
+  config.set(configuration);
 }
