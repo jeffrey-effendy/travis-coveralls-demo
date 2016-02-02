fa6bdc5710a92d838e0c8d21faf0dd3266d88791
Jeffrey Effendy <wawa_union.zone@live.com>
2016-02-02 10:35:36 +0800

    add codecov

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var karma = require('karma');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var codecov = require('gulp-codecov');
+var open = require('gulp-open');
 
 gulp.task('watch', function() {
   livereload.listen();
@@ -31,6 +32,11 @@ gulp.task('frontend-test', function(done) {
   }, done).start();
 });
 
+gulp.task('open-frontend-coverage', ['frontend-test'], function() {
+  return gulp.src('./test/coverage/jasmine/lcov/lcov-report/index.html')
+    .pipe(open({ app: 'chrome' }));
+});
+
 gulp.task('prepare-istanbul-reporter', function() {
   return gulp.src(['server/*.js', 'server/**/*.js'])
     .pipe(istanbul())
@@ -47,10 +53,19 @@ gulp.task('backend-test', ['prepare-istanbul-reporter'], function() {
     })); 
 });
 
-gulp.task('codecov', ['frontend-test', 'backend-test'], function() {
+gulp.task('open-backend-coverage', ['backend-test'], function() {
+  return gulp.src('./test/coverage/mocha/lcov/lcov-report/index.html')
+    .pipe(open({ app: 'chrome' }));
+});
+
+gulp.task('test:local', ['open-frontend-coverage', 'open-backend-coverage'], function() {
+  
+})
+
+gulp.task('test:ci', ['frontend-test', 'backend-test'], function() {
   return gulp.src(['./test/coverage/mocha/lcov/lcov.info', './test/coverage/jasmine/lcov/lcov.info'])
     .pipe(codecov());
 });
 
-gulp.task('default', ['codecov'], function() {
+gulp.task('default', function() {
 });
