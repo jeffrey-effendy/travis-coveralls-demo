27bef7a278a98afd2fe4dac4d383d117981b73c0
Jeffrey Effendy <wawa_union.zone@live.com>
2016-02-02 10:03:40 +0800

    add codecov

--- server/daos/todoDao.js
+++ server/daos/todoDao.js
@@ -5,6 +5,8 @@ var env = process.env.NODE_ENV;
 var table = env === 'test' ? Properties.datasource.testtable : Properties.datasource.table; 
 
 module.exports.fetchOne = function(id, cb, errcb) {
+  cb = cb || function(row, fields){};
+  errcb = errcb || function(err){};
   var statement = "SELECT * FROM " + table + " WHERE id=" + id + " limit 1;";
   return Connection.query(statement, function(rows, fields) {
     cb(rows, fields);
@@ -14,6 +16,8 @@ module.exports.fetchOne = function(id, cb, errcb) {
 }
 
 module.exports.fetchAll = function(cb, errcb) {
+  cb = cb || function(row, fields){};
+  errcb = errcb || function(err){};
   var statement = "SELECT * FROM " + table + ";";
   return Connection.query(statement, function(rows, fields) {
     cb(rows, fields);
@@ -23,6 +27,8 @@ module.exports.fetchAll = function(cb, errcb) {
 }
 
 module.exports.insert = function(description, cb, errcb) {
+  cb = cb || function(row, fields){};
+  errcb = errcb || function(err){};
   var statement = "INSERT INTO " + table + "(description) VALUES (\"" + description + "\") ;";
   return Connection.query(statement, function(rows, fields) {
     cb(rows, fields);
@@ -32,6 +38,8 @@ module.exports.insert = function(description, cb, errcb) {
 }
 
 module.exports.deleteOne = function(id, cb, errcb) {
+  cb = cb || function(row, fields){};
+  errcb = errcb || function(err){};
   var statement = "DELETE FROM " + table + " WHERE id=" + id + ";";
   return Connection.query(statement, function(rows, fields) {
     cb(rows, fields);
